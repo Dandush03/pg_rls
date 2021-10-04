f249695152489bf761184092bcef7547f672f7c9
Daniel Laloush <daniel.laloush@influitive.com>
2021-10-04 12:42:47 -0400

    Basic Setup

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2021-10-04
+
+- Initial release
---
+++ spec/pg_rls_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PgRls do
+  it "has a version number" do
+    expect(PgRls::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
