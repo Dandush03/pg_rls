6a768e081b853135a2ab5b7655c674921a2dc037
Georgie Daniell <georgie@dijanow.com>
2022-01-27 16:24:09 +0000

    Make fetch method work for all pgrls main models Previously fetch was throwing an error as it was calling find_by_tenant_id on an instance of tenant. This should make the method more flexible
    Reset Tenant if tenant id not present in request
    This is to ensure that tenant is reset to null if a request with no
    tenant is made to an instance where tenant has previously been set.

---
+++ lib/pg_rls/errors/tenant_not_found.rb
@@ -0,0 +1,18 @@
+module PgRls
+  module Errors
+    class TenantNotFound < StandardError
+      def initialize
+        reset_tenant_id
+        super
+      end
+
+      def message
+        "Tenant Doesn't exist"
+      end
+
+      def reset_tenant_id
+        PgRls.connection_class.connection.execute('RESET rls.tenant_id')
+      end
+    end
+  end
+end
--- lib/pg_rls/version.rb
+++ lib/pg_rls/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PgRls
-  VERSION = '0.0.1.4.1'
+  VERSION = '0.0.1.4.2'
 end
