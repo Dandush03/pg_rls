fc253b01652c609f455f21616119a40b37da631c
Daniel Laloush <d.laloush@outlook.com>
2023-02-28 13:03:50 -0500

    Fix Midlerware Malfunction

--- lib/pg_rls/middleware/set_reset_connection.rb
+++ lib/pg_rls/middleware/set_reset_connection.rb
@@ -1,6 +1,44 @@
 # frozen_string_literal: true
 
 module PgRls
+  class FrozenConfiguration < StandardError; end
+
+  def self.sessions
+    @sessions ||= Redis.new(@session_store_server)
+  end
+
+  def self.session_prefix
+    @session_prefix ||= begin
+      store_default_warden_key = @session_store_default_warden_key || '2'
+
+      "#{session_key_prefix}:#{store_default_warden_key}::"
+    end
+  end
+
+  def self.session_store_server=(opts = {})
+    raise Errors::FrozenConfiguration unless @sessions.nil?
+
+    @session_store_server = opts.deep_symbolize_keys
+  end
+
+  def self.session_store_default_warden_key=(val)
+    raise Errors::FrozenConfiguration unless @sessions.nil?
+
+    @session_store_default_warden_key = val
+  end
+
+  def self.session_key_prefix
+    @session_key_prefix ||= @session_key_prefix || @session_store_server[:key_prefix]
+  end
+
+  def self.session_key_prefix=(val)
+    raise Errors::FrozenConfiguration unless @sessions.nil?
+
+    @session_key_prefix = val
+  end
+end
+
+module PgRls
   module Middleware
     # Set RLS if sessions present.
     class SetResetConnection
@@ -26,7 +64,7 @@ module PgRls
         cookie_string = env['HTTP_COOKIE']
         return if cookie_string.nil?
 
-        cookie_regex = /#{PgRls.session_key}=([^;]+)/
+        cookie_regex = /#{PgRls.session_key_prefix}=([^;]+)/
         match = cookie_regex.match(cookie_string)
         match[1] if match
       end
@@ -36,8 +74,15 @@ module PgRls
 
         return if cookie.blank?
 
-        sessions = Rails.cache.read("#{PgRls.session_prefix}#{Digest::SHA256.hexdigest(cookie)}")
-        sessions['_tenant'] if sessions.present?
+        redis_session_key = "#{PgRls.session_prefix}#{Digest::SHA256.hexdigest(cookie)}"
+        tenant_session = Marshal.load(PgRls.sessions.get(redis_session_key))
+
+        return if tenant_session.blank?
+        return if tenant_session['_tenant'].blank?
+
+        tenant_session['_tenant'] if tenant_session.present?
+      rescue TypeError
+        nil
       end
 
       def rails_active_storage_request?(env)
