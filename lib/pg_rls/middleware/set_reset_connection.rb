50b1697c3feb634899a6413e626539d7c60f7c02
Daniel Laloush <d.laloush@outlook.com>
2023-11-02 00:40:32 -0500

    test

--- lib/pg_rls/middleware/set_reset_connection.rb
+++ lib/pg_rls/middleware/set_reset_connection.rb
@@ -47,19 +47,23 @@ module PgRls
       end
 
       def call(env)
-        tenant = load_tenant_thought_session(env)
+        PgRls.connection_class.connection_pool.with_connection do
+          begin
+            tenant = load_tenant_thought_session(env)
 
-        return @app.call(env) if tenant.blank?
+            return @app.call(env) if tenant.blank?
 
-        PgRls::Tenant.with_tenant!(tenant) { @app.call(env) }
-      rescue PgRls::Errors::TenantNotFound
-        @app.call(env)
-      rescue ActiveRecord::RecordNotFound => e
-        raise e unless rails_active_storage_request?(env)
+            PgRls::Tenant.with_tenant!(tenant) { @app.call(env) }
+          rescue PgRls::Errors::TenantNotFound
+            @app.call(env)
+          rescue ActiveRecord::RecordNotFound => e
+            raise e unless rails_active_storage_request?(env)
 
-        [404, { 'Content-Type' => 'text/plain' }, ['Could not find asset']]
-      ensure
-        PgRls::Tenant.reset_rls! unless PgRls.test_inline_tenant == true
+            [404, { 'Content-Type' => 'text/plain' }, ['Could not find asset']]
+          ensure
+            PgRls::Tenant.reset_rls! unless PgRls.test_inline_tenant == true
+          end
+        end
       end
 
       def load_session_cookie_value(env)
