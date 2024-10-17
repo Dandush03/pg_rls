59d95cbcb2c26505085807d77794546513690c45
Daniel Laloush <d.laloush@outlook.com>
2024-10-17 12:59:35 -0500

    Fix Cov

--- lib/pg_rls/version.rb
+++ lib/pg_rls/version.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# :nocov:
 module PgRls
   VERSION = "1.0.0"
 end
+# :nocov:
--- test/test_helpers/simplecov.rb
+++ test/test_helpers/simplecov.rb
@@ -16,4 +16,6 @@ SimpleCov.start "rails" do
   formatter SimpleCov::Formatter::MultiFormatter.new([
                                                        SimpleCov::Formatter::HTMLFormatter
                                                      ])
+
+  puts "SimpleCov is running"
 end
