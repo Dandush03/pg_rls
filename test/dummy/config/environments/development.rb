4fef04f59058604d169d4fe2ed9151a2d853b6bf
Daniel Laloush <d.laloush@outlook.com>
2024-10-17 16:54:30 -0500

    Fix Rails Commands

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
+  # config.active_job.verbose_enqueue_logs = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
