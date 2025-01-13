684400a11f6079eb07119cf14a435b535ca8362b
Ricardo Ángeles <ricardo.angeles@skydropx.com>
2025-01-13 17:45:36 -0600

    Update the look_up_connection_config test

--- test/pg_rls_test.rb
+++ test/pg_rls_test.rb
@@ -40,15 +40,15 @@ class PgRlsTest < ActiveSupport::TestCase
     assert_equal expected_defaults, PgRls::DEFAULT_CONFIG_MAP
   end
 
-  test "look_up_connection_config sets connection config based on default if rls mode not dual" do
+  test "look_up_connection_config sets connection config based on default if rls mode is single" do
     db_config = ::ActiveRecord::Base.connection_db_config.configuration_hash.dup
     hash_config = ::ActiveRecord::DatabaseConfigurations::HashConfig.new("test", "primary",
                                                                          db_config.merge(rls_mode: "single"))
 
     ::ActiveRecord::Base.stub :connection_db_config, hash_config do
       config = PgRls.look_up_connection_config
-      assert_equal config[:database][:writing], :primary
-      assert_equal config[:database][:reading], :primary
+      assert_equal config[:database][:writing], :rls_primary
+      assert_equal config[:database][:reading], :rls_primary
     end
   end
 
@@ -59,8 +59,22 @@ class PgRlsTest < ActiveSupport::TestCase
 
     ::ActiveRecord::Base.stub :connection_db_config, hash_config do
       config = PgRls.look_up_connection_config
-      assert_equal config[:database][:writing], :rls_primary
-      assert_equal config[:database][:reading], :rls_primary
+      assert_equal config[:shards][:rls][:writing], :rls_primary
+      assert_equal config[:shards][:rls][:reading], :rls_primary
+      assert_equal config[:shards][:admin][:writing], :primary
+      assert_equal config[:shards][:admin][:reading], :primary
+    end
+  end
+
+  test "look_up_connection_config sets connection config based on default if rls mode is none" do
+    db_config = ::ActiveRecord::Base.connection_db_config.configuration_hash.dup
+    hash_config = ::ActiveRecord::DatabaseConfigurations::HashConfig.new("test", "primary",
+                                                                          db_config.merge(rls_mode: "none"))
+
+    ::ActiveRecord::Base.stub :connection_db_config, hash_config do
+      config = PgRls.look_up_connection_config
+      assert_equal config[:database][:writing], :primary
+      assert_equal config[:database][:reading], :primary
     end
   end
 
