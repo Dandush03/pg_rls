a1266c8ca7f651a50a1d5589cc748e7d2223de55
Daniel Laloush <d.laloush@outlook.com>
2025-07-15 05:04:33 -0500

    Add param History

--- app/models/pg_rls/current.rb
+++ app/models/pg_rls/current.rb
@@ -35,11 +35,11 @@ module PgRls
 
     def add_tenant_to_history
       @attributes[:tenant_history] ||= []
-      @attributes[:tenant_history] << @attributes[:tenant] unless @attributes[:tenant].nil?
+      @attributes[:tenant_history] << @attributes unless @attributes[:tenant].nil?
     end
 
     def restore_most_recent_tenant
-      @attributes[:tenant] = @attributes[:tenant_history].pop
+      @attributes = @attributes[:tenant_history].pop if @attributes[:tenant_history].any?
       return PgRls::Tenant.reset_rls_used_connections if @attributes[:tenant].nil?
 
       @attributes[:tenant].set_rls
